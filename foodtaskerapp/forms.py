b7f080596f74b6ba2fc9bece45c04ccd8a2431c4
Darien Duong <darien@Dariens-MacBook-Air.local>
2017-10-13 18:39:06 -0700

    push up changes

--- foodtaskerapp/forms.py
+++ foodtaskerapp/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from foodtaskerapp.models import Restaurant
+from foodtaskerapp.models import Restaurant, Meal
 
 class UserForm(forms.ModelForm):
     class Meta:
@@ -9,7 +9,18 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ("username", "password", "first_name", "last_name", "email")
 
+class UserFormForEdit(forms.ModelForm):
+    class Meta:
+        email = forms.CharField(max_length=100, required=True)
+        model = User
+        fields = ("first_name", "last_name", "email")
+
 class RestaurantForm(forms.ModelForm):
     class Meta:
         model = Restaurant
         fields = ("name", "phone", "address", "logo")
+
+class MealForm(forms.ModelForm):
+    class Meta:
+        model = Meal
+        exclude = ('restaurant',)
